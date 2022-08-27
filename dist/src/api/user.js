37fb2c7900918e8d07bd996b1e68b350ae09df9e
Eduard Dyckman <bird.duskpoet@gmail.com>
2022-08-27 16:45:06 +0300

    Remove dist

--- dist/src/api/user.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.me = void 0;
-const gateway_1 = require("./gateway");
-const me = (props) => (0, gateway_1.apiCall)(Object.assign(Object.assign({}, props), { path: 'me', method: 'GET' }));
-exports.me = me;
