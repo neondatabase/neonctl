e3520558662ea2bc8117e5c4c94d4f7625c67efd
Eduard Dyckman <bird.duskpoet@gmail.com>
2023-06-28 16:40:40 +0300

    fix: remove endpoints

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,7 +2,6 @@ import * as auth from './auth.js';
 import * as projects from './projects.js';
 import * as users from './user.js';
 import * as branches from './branches.js';
-import * as endpoints from './endpoints.js';
 import * as databases from './databases.js';
 import * as roles from './roles.js';
 import * as operations from './operations.js';
@@ -13,7 +12,6 @@ export default [
   users,
   projects,
   branches,
-  endpoints,
   databases,
   roles,
   operations,
