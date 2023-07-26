2cd7de2a84e8eb11628271bfdd3b027267e6f585
Markus Machatschek <markus.machatschek@hey.com>
2023-07-26 12:20:16 +0200

    test: passing branch ids with 8 digit suffix should work

---
+++ mocks/main/projects/test/branches/br-cloudy-branch-12345678/roles/POST.js
@@ -0,0 +1,14 @@
+export default function (req, res) {
+  expect(req.body).toMatchObject({
+    role: {
+      name: 'test_role',
+    },
+  });
+
+  res.send({
+    role: {
+      name: 'test_role',
+      created_at: '2019-01-01T00:00:00.000Z',
+    },
+  });
+}
