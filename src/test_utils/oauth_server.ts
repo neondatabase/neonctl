33ea186791f1a2cf57cc65f5f3a411be3533008c
Eduard Dyckman <bird.duskpoet@gmail.com>
2023-07-31 13:09:52 +0300

    fix: tests

--- src/test_utils/oauth_server.ts
+++ src/test_utils/oauth_server.ts
@@ -4,7 +4,7 @@ import { OAuth2Server } from 'oauth2-mock-server';
 export const startOauthServer = async () => {
   const server = new OAuth2Server();
   await server.issuer.keys.generate('RS256');
-  await server.start(7777, 'localhost');
+  await server.start(0, 'localhost');
   log.info('Started OAuth server');
   return server;
 };
