5f2ad567af0e1ad7de552709b8e2f09b0d3d6ec2
Amit Dahan <amitdahan11@gmail.com>
2023-11-30 10:30:43 +0200

    Run prettier

--- mocks/bin/psql.cjs
+++ mocks/bin/psql.cjs
@@ -3,7 +3,7 @@
 // A psql mock that just prints its arguments (as an array in an object) to stdout.
 // Node.js script should have an extension (otherwise ERR_UNKNOWN_FILE_EXTENSION is thrown), so we use a symlink psql -> psql.cjs
 if (require.main === module) {
-    process.stdout.write(
-        JSON.stringify({'psql-cli-args': process.argv.slice(2)})
-    );
+  process.stdout.write(
+    JSON.stringify({ 'psql-cli-args': process.argv.slice(2) })
+  );
 }
