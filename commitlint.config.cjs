3d9c7e4e6477393b81bcda11485d75afb5b7c9b2
Eduard Dyckman <bird.duskpoet@gmail.com>
2023-12-08 14:31:02 +0200

    fix: commitlint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'footer-max-line-length': [0, 'always', 100],
+    'body-max-line-length': [0, 'always', 100],
   },
 };
