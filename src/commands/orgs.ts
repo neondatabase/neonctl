6c652b92ffae1e710129465f4da87d74d97e8f57
Amit Dahan <amitdahan11@gmail.com>
2024-05-26 14:43:25 +0300

    feat: support `neonctl orgs list`

---
+++ src/commands/orgs.ts
@@ -0,0 +1,38 @@
+import yargs from 'yargs';
+
+import { CommonProps } from '../types.js';
+import { writer } from '../writer.js';
+
+const ORG_FIELDS = ['id', 'handle', 'name'] as const;
+
+export const command = 'orgs';
+export const describe = 'Manage organizations';
+export const aliases = ['org'];
+export const builder = (argv: yargs.Argv) => {
+  return argv.usage('$0 orgs <sub-command> [options]').command(
+    'list',
+    'List organizations',
+    (yargs) => yargs,
+    async (args) => {
+      // @ts-expect-error: TODO - Assert `args` is `CommonProps`
+      await list(args);
+    },
+  );
+};
+export const handler = (args: yargs.Argv) => {
+  return args;
+};
+
+const list = async (props: CommonProps) => {
+  const out = writer(props);
+
+  const {
+    data: { organizations },
+  } = await props.apiClient.getCurrentUserOrganizations();
+
+  out.write(organizations, {
+    fields: ORG_FIELDS,
+    title: 'Organizations',
+  });
+  out.end();
+};
