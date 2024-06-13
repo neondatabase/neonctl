b7dce12381d191c057da38159e14d4b1cfef1fc9
Amit Dahan <amitdahan11@gmail.com>
2024-06-13 10:47:16 +0300

    Add tests

--- mocks/main/projects/POST.js
+++ mocks/main/projects/POST.js
@@ -30,6 +30,28 @@ export default function (req, res) {
       },
     });
   }
+
+  if (req.body.project.org_id) {
+    expect(req.body.project.org_id).toBe('org-2');
+
+    return res.json({
+      project: {
+        id: 'new-project-789012',
+        name: 'test_project',
+        created_at: '2022-01-01T00:00:00.000Z',
+        org_id: 'org-2',
+      },
+      connection_uris: [
+        { connection_uri: 'postgres://localhost:5432/test_project' },
+      ],
+      branch: {
+        id: 'br-test-branch-789012',
+        name: 'test_branch',
+        created_at: '2022-01-01T00:00:00.000Z',
+      },
+    });
+  }
+
   res.send({
     project: {
       id: 'new-project-123456',
