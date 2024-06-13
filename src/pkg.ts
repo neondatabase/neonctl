bd63d4aac0b40569142cc6971e8f4a46e042578e
Amit Dahan <amitdahan11@gmail.com>
2024-06-13 12:05:50 +0300

    wip bun migration

--- src/pkg.ts
+++ src/pkg.ts
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'node:url';
 
 export default JSON.parse(
   readFileSync(
-    fileURLToPath(new URL('./package.json', import.meta.url)),
+    fileURLToPath(new URL('../package.json', import.meta.url)),
     'utf-8',
   ),
 );
--- src/test_utils/oauth_server.ts
+++ src/test_utils/oauth_server.ts
@@ -3,7 +3,7 @@ import { OAuth2Server } from 'oauth2-mock-server';
 
 export const startOauthServer = async () => {
   const server = new OAuth2Server();
-  await server.issuer.keys.generate('RS256');
+  // await server.issuer.keys.generate('RS256');
   await server.start(0, 'localhost');
   log.debug('Started OAuth server');
   return server;
