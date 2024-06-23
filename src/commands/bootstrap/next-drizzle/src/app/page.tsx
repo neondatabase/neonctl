031eb92916c8a6c80a9e671c509947f8509fb6e4
David Gomes <davidrafagomes@gmail.com>
2024-06-23 19:17:10 +0200

    feat: support next+drizzle without auth

---
+++ src/commands/bootstrap/next-drizzle/src/app/page.tsx
@@ -0,0 +1,5 @@
+import styles from './page.module.css';
+
+export default async function Home() {
+  return <main className={styles.main}>Hello World!</main>;
+}
---
+++ src/commands/bootstrap/next-drizzle/src/lib/schema.ts
@@ -0,0 +1,1 @@
+// Define your app's schema here!
