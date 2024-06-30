46230c4cb3af07b67a1c66667eead36ebde0ff65
David Gomes <davidrafagomes@gmail.com>
2024-06-30 07:12:42 +0200

    feat: new templates and cloudflare deployment

--- src/commands/bootstrap/next-drizzle-authjs/drizzle.config.ts
+++ src/commands/bootstrap/next-drizzle-authjs/drizzle.config.ts
@@ -1,6 +1,4 @@
 import { defineConfig } from "drizzle-kit";
-import dotenv from "dotenv";
-dotenv.config({ path: [".env.local", ".env"] });
 
 export default defineConfig({
   schema: "./src/lib/schema.ts",
