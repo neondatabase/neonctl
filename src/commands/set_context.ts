24fc91d9f94945e74bc3067eebc875604fb92bc2
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2024-07-03 17:42:53 +0200

    fix: remove storing and enriching branch to/from context (#231)
    - [x] Remove storing `branchId` to context on `set-context` command
    - [x] Still enrich commands with `projectId` and `branchId` from
    context, for backward compatibility
    - [x] Enrich commands with `branchId` only if the `projectId` is from
    the context.

--- src/commands/set_context.ts
+++ src/commands/set_context.ts
@@ -1,6 +1,5 @@
 import yargs from 'yargs';
 import { Context, updateContextFile } from '../context.js';
-import { branchIdFromProps } from '../utils/enrichers.js';
 import { BranchScopeProps } from '../types.js';
 
 export const command = 'set-context';
@@ -11,17 +10,11 @@ export const builder = (argv: yargs.Argv) =>
       describe: 'Project ID',
       type: 'string',
     },
-    branch: {
-      describe: 'Branch ID or name',
-      type: 'string',
-    },
   });
 
 export const handler = async (props: BranchScopeProps) => {
-  const branchId = await branchIdFromProps(props);
   const context: Context = {
     projectId: props.projectId,
-    branchId,
   };
   updateContextFile(props.contextFile, context);
 };
