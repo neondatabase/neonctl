75e274346b539882e05d994aa3a2b0c842ec527e
Amit Dahan <amitdahan11@gmail.com>
2024-07-04 09:15:54 +0300

    fallback to package.json from parent dir

--- src/pkg.ts
+++ src/pkg.ts
@@ -1,9 +1,20 @@
-import { readFileSync } from 'node:fs';
+import { readFileSync, existsSync } from 'node:fs';
+import { dirname, join, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-export default JSON.parse(
-  readFileSync(
-    fileURLToPath(new URL('./package.json', import.meta.url)),
-    'utf-8',
-  ),
-);
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const currPath = join(__dirname, 'package.json');
+const parentPath = resolve(__dirname, '..', 'package.json');
+
+const pkgJsonPath = existsSync(currPath)
+  ? currPath
+  : existsSync(parentPath)
+    ? parentPath
+    : undefined;
+
+if (!pkgJsonPath) {
+  throw new Error('package.json not found');
+}
+
+export default JSON.parse(readFileSync(pkgJsonPath, 'utf-8'));
