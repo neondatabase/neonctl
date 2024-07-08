2243f99550a04dd244f7bc08f9fb934faaadd6be
Amit Dahan <amitdahan11@gmail.com>
2024-07-08 09:22:20 +0300

    Remove custom snapshot path logic

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,5 @@
-import { join, relative, sep } from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    setupFiles: ['./test-setup.ts'],
-    resolveSnapshotPath(testPath, snapshotExtension) {
-      const p = relative(__dirname, testPath);
-      const parts = p.split(sep);
-      parts[0] = 'snapshots';
-      parts[parts.length - 1] = parts[parts.length - 1].replace(
-        '.ts',
-        snapshotExtension,
-      );
-      return join(__dirname, ...parts);
-    },
-  },
+  test: { setupFiles: ['./test-setup.ts'] },
 });
