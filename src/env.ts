840df14da8b7039a35fa85a6e07a10af4b4c68fc
antelman107 <anton_elmanov@epam.com>
2024-10-07 14:21:16 +0300

    feat: add github environment tracking to Segment

--- src/env.ts
+++ src/env.ts
@@ -5,3 +5,35 @@ export const isCi = () => {
 export const isDebug = () => {
   return Boolean(process.env.DEBUG);
 };
+
+export const getGithubEnvironmentVars = () => {
+  const vars = [
+    'GITHUB_ACTION',
+    'GITHUB_ACTION_PATH',
+    'GITHUB_ACTION_REPOSITORY',
+    'GITHUB_TRIGGERING_ACTOR',
+    'GITHUB_ACTOR',
+    'GITHUB_ACTOR_ID',
+    'GITHUB_EVENT_NAME',
+    'GITHUB_RUN_NUMBER',
+
+    'GITHUB_REF_TYPE',
+    'GITHUB_REF',
+    'GITHUB_REF_NAME',
+    'GITHUB_BASE_REF',
+    'GITHUB_HEAD_REF',
+    'GITHUB_JOB',
+    'GITHUB_SHA',
+    'GITHUB_REPOSITORY',
+    'GITHUB_REPOSITORY_ID',
+    'GITHUB_REPOSITORY_OWNER',
+    'GITHUB_REPOSITORY_OWNER_ID',
+
+    'GITHUB_WORKFLOW',
+    'GITHUB_WORKFLOW_REF',
+    'GITHUB_WORKFLOW_SHA',
+  ];
+  return vars.map((item) => {
+    return { item: process.env[item] };
+  });
+};
