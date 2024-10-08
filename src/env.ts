282adc9f20ddef1db3dca16d29dd7404e9702227
Anton Elmanov <45532870+antelman107@users.noreply.github.com>
2024-10-08 15:40:13 +0300

    feat: add github environment tracking to Segment (#286)
    https://github.com/neondatabase/cloud/issues/13352

--- src/env.ts
+++ src/env.ts
@@ -5,3 +5,44 @@ export const isCi = () => {
 export const isDebug = () => {
   return Boolean(process.env.DEBUG);
 };
+
+export const getGithubEnvVars = (env: Dict<string>) => {
+  const vars = [
+    // github action info
+    'GITHUB_ACTION',
+    'GITHUB_ACTION_PATH',
+    'GITHUB_ACTION_REPOSITORY',
+
+    // source github repository and actor info
+    'GITHUB_REF_TYPE',
+    'GITHUB_REF',
+    'GITHUB_REF_NAME',
+    'GITHUB_BASE_REF',
+    'GITHUB_HEAD_REF',
+    'GITHUB_JOB',
+    'GITHUB_SHA',
+    'GITHUB_REPOSITORY',
+    'GITHUB_REPOSITORY_ID',
+    'GITHUB_REPOSITORY_OWNER',
+    'GITHUB_REPOSITORY_OWNER_ID',
+    'GITHUB_TRIGGERING_ACTOR',
+    'GITHUB_ACTOR',
+    'GITHUB_ACTOR_ID',
+    'GITHUB_EVENT_NAME',
+    'GITHUB_RUN_NUMBER',
+
+    // reusable workflow info
+    'GITHUB_WORKFLOW',
+    'GITHUB_WORKFLOW_REF',
+    'GITHUB_WORKFLOW_SHA',
+  ];
+
+  const map = new Map();
+  vars.forEach((v) => {
+    if (env[v]) {
+      map.set(v, env[v]);
+    }
+  });
+
+  return Object.fromEntries(map);
+};
