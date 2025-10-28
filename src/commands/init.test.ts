93abcad6ccabe9e498dcb2ad868f4b63f1f567eb
Rodney Shibu <rodneysherwinshibu@gmail.com>
2025-10-28 17:58:24 +0100

    test: update test

--- src/commands/init.test.ts
+++ src/commands/init.test.ts
@@ -5,8 +5,4 @@ describe('init', () => {
   test('init should run neon-init', async ({ testCliCommand }) => {
     await testCliCommand(['init']);
   });
-
-  test('init with an argument', async ({ testCliCommand }) => {
-    await testCliCommand(['init', '--', '--debug']);
-  });
 });
