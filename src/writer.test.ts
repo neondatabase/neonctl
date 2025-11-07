1482578c0ff1f34a523f8089ea2765cb2d6454df
Eduard Dykman <bird.duskpoet@gmail.com>
2025-11-07 16:42:24 +0200

    feat: branches list displays meta icons
    fix: replace emoji with "anon"
    
    fix: tests mock writer

--- src/writer.test.ts
+++ src/writer.test.ts
@@ -1,98 +1,122 @@
-import { Writable } from 'node:stream';
+import { PassThrough } from 'node:stream';
 import { describe, it, expect } from 'vitest';
 import { writer } from './writer.js';
 
-class MockWritable extends Writable {
-  _data: Buffer[] = [];
+const getMockWritable = () => {
+  const chunks: string[] = [];
+  const stream = new PassThrough();
+  stream.on('data', (chunk) => {
+    chunks.push(chunk.toString());
+  });
 
-  get data() {
-    return this._data.map((chunk) => chunk.toString()).join('');
-  }
-
-  _write(chunk: Buffer) {
-    this._data.push(chunk);
-  }
-}
+  return {
+    stream,
+    getData: () => {
+      return chunks.join('');
+    },
+  };
+};
 
 describe('writer', () => {
   describe('outputs yaml', () => {
     it('outputs single data', () => {
-      const stream = new MockWritable();
+      const { stream, getData } = getMockWritable();
       const out = writer({ output: 'yaml', out: stream });
       out.end({ foo: 'bar' }, { fields: ['foo'] });
-      expect(stream.data).toMatchSnapshot();
+      expect(getData()).toMatchSnapshot();
     });
 
     it('outputs single data with title', () => {
-      const stream = new MockWritable();
+      const { stream, getData } = getMockWritable();
       const out = writer({ output: 'yaml', out: stream });
       out.end({ foo: 'bar' }, { fields: ['foo'], title: 'baz' });
-      expect(stream.data).toMatchSnapshot();
+      expect(getData()).toMatchSnapshot();
     });
 
     it('outputs multiple data', () => {
-      const stream = new MockWritable();
+      const { stream, getData } = getMockWritable();
       const out = writer({ output: 'yaml', out: stream });
       out
         .write({ foo: 'bar' }, { fields: ['foo'], title: 'T1' })
         .write({ baz: 'xyz' }, { fields: ['baz'], title: 'T2' })
         .end();
-      expect(stream.data).toMatchSnapshot();
+      expect(getData()).toMatchSnapshot();
     });
   });
 
   describe('outputs json', () => {
     it('outputs single data', () => {
-      const stream = new MockWritable();
+      const { stream, getData } = getMockWritable();
       const out = writer({ output: 'json', out: stream });
       out.end({ foo: 'bar' }, { fields: ['foo'] });
-      expect(stream.data).toMatchSnapshot();
+      expect(getData()).toMatchSnapshot();
     });
 
     it('outputs single data with title', () => {
-      const stream = new MockWritable();
+      const { stream, getData } = getMockWritable();
       const out = writer({ output: 'json', out: stream });
       out.end({ foo: 'bar' }, { fields: ['foo'], title: 'baz' });
-      expect(stream.data).toMatchSnapshot();
+      expect(getData()).toMatchSnapshot();
     });
 
     it('outputs multiple data', () => {
-      const stream = new MockWritable();
+      const { stream, getData } = getMockWritable();
       const out = writer({ output: 'json', out: stream });
       out
         .write({ foo: 'bar' }, { fields: ['foo'], title: 'T1' })
         .write({ baz: 'xyz' }, { fields: ['baz'], title: 'T2' })
         .end();
-      expect(stream.data).toMatchSnapshot();
+      expect(getData()).toMatchSnapshot();
     });
   });
 
   describe('outputs table', () => {
     it('outputs single data', () => {
-      const stream = new MockWritable();
+      const { stream, getData } = getMockWritable();
       const out = writer({ output: 'table', out: stream });
       out.end({ foo: 'bar', extra: 'extra' }, { fields: ['foo'] });
-      expect(stream.data).toMatchSnapshot();
+      expect(getData()).toMatchSnapshot();
     });
 
     it('outputs single data with title', () => {
-      const stream = new MockWritable();
+      const { stream, getData } = getMockWritable();
       const out = writer({ output: 'table', out: stream });
       out.end(
         { foo: 'bar', extra: 'extra' },
         { fields: ['foo'], title: 'baz' },
       );
-      expect(stream.data).toMatchSnapshot();
+      expect(getData()).toMatchSnapshot();
     });
 
     it('outputs multiple data', () => {
-      const stream = new MockWritable();
+      const { stream, getData } = getMockWritable();
       const out = writer({ output: 'table', out: stream });
       out
         .write({ foo: 'bar', extra: 'extra' }, { fields: ['foo'], title: 'T1' })
         .write({ baz: 'xyz', extra: 'extra' }, { fields: ['baz'], title: 'T2' })
         .end();
-      expect(stream.data).toMatchSnapshot();
+      expect(getData()).toMatchSnapshot();
+    });
+
+    it('outputs table with custom renderer', () => {
+      const { stream, getData } = getMockWritable();
+      const out = writer({
+        output: 'table',
+        out: stream,
+      });
+      out
+        .write(
+          { foo: 'bar' },
+          {
+            fields: ['foo'],
+            title: 'T1',
+            renderColumns: {
+              foo: ({ foo }) => `Here is: ${foo}`,
+            },
+          },
+        )
+        .end();
+      expect(getData()).toMatchSnapshot();
     });
   });
 });
