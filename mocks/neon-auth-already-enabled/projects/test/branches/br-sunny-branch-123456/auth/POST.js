1838e3ddeada528d790f1923df8ea935148d7604
Jeff Christoffersen <jeffrey@neon.tech>
2026-06-03 12:40:25 -0600

    style: fix prettier formatting in mock file
    Co-authored-by: Isaac

--- mocks/neon-auth-already-enabled/projects/test/branches/br-sunny-branch-123456/auth/POST.js
+++ mocks/neon-auth-already-enabled/projects/test/branches/br-sunny-branch-123456/auth/POST.js
@@ -1,3 +1,5 @@
 export default function (req, res) {
-  res.status(409).send({ message: 'Neon Auth is already enabled for this branch' });
+  res
+    .status(409)
+    .send({ message: 'Neon Auth is already enabled for this branch' });
 }
